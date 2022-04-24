4d81d740ccd8bd5033ef785c5a4be6b754c3d6ad
Ali Ateş <dev.ates@hotmail.com>
2022-04-24 12:02:50 +0300

    Tüm alt bölümleri kapsar.

---
+++ src/Ates/ServiceRegister.cs
@@ -0,0 +1,11 @@
+﻿namespace Ates;
+using Ates.Tool;
+using Microsoft.Extensions.DependencyInjection;
+public static class ServiceRegister
+{
+    public static IServiceCollection AddAtesServices(this IServiceCollection services)
+    {
+        return services
+            .AddAtesToolsServices();
+    }
+}
