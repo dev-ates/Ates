726c95071d2142288286f7c3343495708b9d75b5
Ali Ateş <dev.ates@hotmail.com>
2022-05-09 23:54:10 +0300

    Fluent Validator Service eklendi.

---
+++ src/Ates.AspNetCore/ServiceRegister.cs
@@ -0,0 +1,23 @@
+﻿namespace Ates.AspNetCore;
+
+using Ates.AspNetCore.Filters;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceRegister
+{
+    public static IServiceCollection AddAtesAspNetCoreServices<TFluentValidatorAssembly>(this IServiceCollection services, TFluentValidatorAssembly assembly)
+    {
+        _ = services
+            .AddControllers(options => options.Filters.Add<ValidationFilter>())
+            .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<TFluentValidatorAssembly>())
+            .ConfigureApiBehaviorOptions(o => o.SuppressModelStateInvalidFilter = true);
+
+        return services;
+    }
+
+    public static IServiceCollection AddAtesAspNetCoreServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
--- src/Ates/ServiceRegister.cs
+++ src/Ates/ServiceRegister.cs
@@ -1,11 +1,19 @@
 ﻿namespace Ates;
 using Ates.Tool;
+using Ates.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 public static class ServiceRegister
 {
     public static IServiceCollection AddAtesServices(this IServiceCollection services)
     {
         return services
+            .AddAtesToolsServices();
+    }
+
+    public static IServiceCollection AddAtesServices<TFluentValidatorAssembly>(this IServiceCollection services, TFluentValidatorAssembly assembly)
+    {
+        return services
+            .AddAtesAspNetCoreServices(assembly)
             .AddAtesToolsServices();
     }
 }
