2d5ff7f8a2a0d1586471b584a06bbd91663b07e8
Ali Ateş <dev.ates@hotmail.com>
2022-05-10 01:08:57 +0300

    Upload yapıldı.

--- src/Ates.AspNetCore/Filters/ValidationFilter.cs
+++ src/Ates.AspNetCore/Filters/ValidationFilter.cs
@@ -9,8 +9,8 @@ public class ValidationFilter : IAsyncActionFilter
         if (!context.ModelState.IsValid)
         {
             var errors = context.ModelState
-                .Where(x => x.Value.Errors.Count > 0)
-                .ToDictionary(e => e.Key, e => e.Value.Errors.Select(e => new
+                .Where(x => x.Value?.Errors.Count > 0)
+                .ToDictionary(e => e.Key, e => e.Value?.Errors.Select(e => new
                 {
                     e.ErrorMessage
                 })).ToArray();
