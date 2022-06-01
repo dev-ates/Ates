5446e779c5c8afdb6ecf76a039d3c4e307593a61
Ali Ateş <dev.ates@hotmail.com>
2022-06-01 21:51:25 +0300

    Test düzeltme.

--- test/Ates.Datas.Test/Repositories/UserReadRepository.cs
+++ test/Ates.Datas.Test/Repositories/UserReadRepository.cs
@@ -3,8 +3,9 @@
 using Ates.Datas.EntityFrameworkCore;
 using Ates.Datas.Test.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
 
-internal class UserReadRepository : ReadRepository<User>
+internal class UserReadRepository : ReadRepository<User, Guid>
 {
     public UserReadRepository(DbContext context) : base(context)
     {
