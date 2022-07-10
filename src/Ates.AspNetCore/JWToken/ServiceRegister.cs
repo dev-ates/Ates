295f0aeffbfb58a9fd067110c3a2b68fe24f405c
Ali Ateş <dev.ates@hotmail.com>
2022-07-10 21:51:16 +0300

    ServiceRegister eklemesi yapıldı.

---
+++ src/Ates.AspNetCore/JWToken/ServiceRegister.cs
@@ -0,0 +1,33 @@
+﻿namespace Ates.AspNetCore.JWToken;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+public static class ServiceRegister
+{
+    public static IServiceCollection JWTAddServices(this IServiceCollection services, String key)
+    {
+        services.AddSingleton<JWTAuthenticationManager>(new JWTAuthenticationManager(key));
+
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+            };
+        });
+
+        return services;
+    }
+}
