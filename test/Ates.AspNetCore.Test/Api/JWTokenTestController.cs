ea9572a915f2170b86ba3df806675c94f9755e3a
Ali Ateş <dev.ates@hotmail.com>
2022-07-10 22:40:21 +0300

    JWToken testleri eklendi.

---
+++ test/Ates.AspNetCore.Test/Api/JWTokenTestController.cs
@@ -0,0 +1,36 @@
+﻿namespace Ates.AspNetCore.Test.Api;
+
+using Ates.AspNetCore.JWToken;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class JWTokenTestController : ControllerBase
+{
+    private readonly JWTAuthenticationManager jWTAuthenticationManager;
+
+    public JWTokenTestController(JWTAuthenticationManager jWTAuthenticationManager)
+    {
+        this.jWTAuthenticationManager = jWTAuthenticationManager;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate(String username, String password)
+    {
+        var token = this.jWTAuthenticationManager.Authenticate(username, password);
+
+        if (token == null)
+            return Unauthorized();
+
+        return Ok(token);
+    }
+
+    [HttpGet("deneme")]
+    public IActionResult Deneme()
+    {
+        return Ok("Başarılı.......");
+    }
+}
