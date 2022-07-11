8d77dfbf251bd160b8620414f9c605ceba85cf9d
Ali Ateş <dev.ates@hotmail.com>
2022-07-11 12:12:55 +0300

    Mail servis için ayarlar eklendi.

---
+++ src/Ates.AspNetCore/MailService/MailServiceSettings.cs
@@ -0,0 +1,33 @@
+﻿namespace Ates.AspNetCore.MailService;
+public class MailServiceSettings
+{
+    public String MailAddress
+    {
+        get; set;
+    }
+
+    public String MailPassword
+    {
+        get; set;
+    }
+
+    public String VerificationMailDisplay
+    {
+        get; set;
+    }
+
+    public Int32 Port
+    {
+        get; set;
+    }
+
+    public String Host
+    {
+        get; set;
+    }
+
+    public Boolean EnableSsl
+    {
+        get; set;
+    }
+}
