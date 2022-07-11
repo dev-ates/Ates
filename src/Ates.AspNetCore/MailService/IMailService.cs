743f903c9dd0dc15ed4b5a137e36aa0738928c33
Ali Ateş <dev.ates@hotmail.com>
2022-07-11 12:11:45 +0300

    Mail servisi eklendi.

---
+++ src/Ates.AspNetCore/MailService/IMailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Ates.AspNetCore.MailService;
+
+public interface IMailService
+{
+    public Task SendMail(String subject, String body, String toMailAddress, CancellationToken cancellationToken = default);
+}
